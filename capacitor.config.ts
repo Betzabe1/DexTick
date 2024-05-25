f928d486e228bac05ff4ae83805db6758968d04f
Betza1 <betzabe.456reyes@gmail.com>
2024-05-25 09:38:12 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DexTick',
+  webDir: 'www'
+};
+
+export default config;
